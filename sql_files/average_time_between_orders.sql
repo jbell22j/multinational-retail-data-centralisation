1ffbfef5d7b5c3b977033f057cb95f63e175d643
Josh.Bell <joshualexbell@gmail.com>
2023-10-23 16:38:14 +0100

    Final query file completed.

--- sql_files/average_time_between_orders.sql
+++ sql_files/average_time_between_orders.sql
@@ -1,10 +1,23 @@
-SELECT AVG(next_order::TIME) AS actual_time_taken,
-year
-FROM(SELECT 
-	year,
-	LEAD(timestamp, 1) OVER (
-		ORDER BY timestamp) AS next_order
+SELECT AVG(time_till_next) AS actual_time_taken,
+		year
 FROM 
-	dim_date_times
-ORDER BY next_order DESC)
-GROUP BY year;
+(SELECT timestamp-next_order AS time_till_next,
+year
+FROM 
+(SELECT 
+    timestamp,
+	LEAD(timestamp,1) OVER (ORDER BY timestamp DESC) AS next_order,
+	year
+FROM
+(SELECT 
+	TO_TIMESTAMP(timestamp,'HH24:MI:SS DD/MM/YYYY') AS timestamp,
+	year
+FROM
+(WITH datetime_CTE AS (SELECT CAST(timestamp AS VARCHAR),
+CONCAT(day,'/',month,'/',year) AS date,
+year
+FROM dim_date_times)
+SELECT CONCAT(timestamp,' ',date) AS timestamp ,year
+FROM datetime_CTE))))
+GROUP BY year
+ORDER BY actual_time_taken DESC;
